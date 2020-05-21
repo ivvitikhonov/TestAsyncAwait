591e37669d535840b3170420e04c10a41b91b551
Ivan Tikhonov <quiteman@mail.ru>
2020-05-21 18:12:13 +0300

    add project files

---
+++ TestAsyncAwait/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestAsyncAwait
+{
+    class Program
+    {
+        //для примеров 1-3 заменить "async Task" на void
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"Main. Start thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+                //#1
+                //Несколько последовательных длительных асинхронных операций. 
+                //Возвращаемое значение не предоставляет интереса.
+                //Test.RunAsyncVersion1();
+
+                //#2
+                //Несколько последовательных длительных операций. 
+                //Возвращаемое значение не предоставляет интереса.
+                //НО есть необходимость выполнить некоторые действия только после завершения всех!!! задач асинхронного метода -
+                //в данном случае код после await AsyncMethod();
+                //Test.RunAsyncVersion2();
+
+                //#3.1
+                //К примеру #1 добавляется синхронная операция
+                Test.RunAsyncVersion31();
+
+                //#3.2
+                //К примеру #2 добавляется синхронная операция
+                //Test.RunAsyncVersion32();
+
+
+                //#4.1. Возвращаемое значение имеет место быть, но в главной программе он не важно/не используется
+                //при вызове аcинхронного метода await не используется
+                //var result1 = Test.RunAsyncVersion41();
+                //Console.WriteLine($"Test.RunAsyncVersion4 result : {result1}");
+
+                //#4.2. Возвращаемое значение имеет место быть, в главной программе он важно/используется
+                //при вызове аcинхронного метода await используется
+                //в вызываемом методе используются дополнительные преобразования
+                //var result2 = await Test.RunAsyncVersion41();
+                //Console.WriteLine($"Test.RunAsyncVersion4 result : {result2}");
+
+                //#4.3. Метод Test.RunAsyncVersion42 не помечен как асинхронный
+                //в вызываемом методе не используются дополнительные преобразования
+                //var result3 = await Test.RunAsyncVersion42();
+                //Console.WriteLine($"Test.RunAsyncVersion4 result : {result3}");
+
+                Console.WriteLine($"Main. Finish thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            }
+            catch (System.AggregateException e)
+            {
+                //Все исключения в TPL пробрасываются обернутые в AggregateException
+                Console.WriteLine("AggregateException: {0}", e.InnerException.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestAsyncAwait/Test.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestAsyncAwait
+{
+    public static  class Test
+    {
+        /// <summary>
+        /// 1) заходим в AsyncMethod, доходим до (1), возвращает управление сюда,
+        /// 2) отображаем сообщение After ..., больше выполнять нечего - передаем управление в Main
+        /// 3) завершаем все действия в Main
+        /// </summary>
+        public static void RunAsyncVersion1()
+        {
+            Console.WriteLine($"\tRunAsyncVersion1. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            AsyncMethod();
+            Console.WriteLine($"\tRunAsyncVersion1. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        /// <summary>
+        /// 1) заходим в AsyncMethod, доходим до (1), возвращает управление сюда,
+        /// 2) завершаем все действия в Main
+        /// 3) завершаем асинхронные задачи webRequest1 и webRequest2
+        /// 4) отображаем After AsyncMethod ...
+        /// </summary>
+        public static async Task RunAsyncVersion2()
+        {
+            Console.WriteLine($"\tRunAsyncVersion2. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            await AsyncMethod();
+            Console.WriteLine($"\tRunAsyncVersion2. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static void RunAsyncVersion31()
+        {
+            Console.WriteLine($"\tRunAsyncVersion3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            var task = AsyncMethod();
+
+            var webRequest3 = WebRequest.Create("http://blogs.msdn.com");
+            Console.WriteLine($"\tWebRequest3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            webRequest3.GetResponse();
+            Console.WriteLine($"\tWebRequest3. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            
+            //если раскомментить, то ожидаем, что все задачи выполнены, только потом идем в Main
+            //task.Wait();
+
+            Console.WriteLine($"\tRunAsyncVersion3. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+
+        public static void RunAsyncVersion32()
+        {
+            Console.WriteLine($"\tRunAsyncVersion3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            var task = AsyncMethod();
+
+            var webRequest3 = WebRequest.Create("http://blogs.msdn.com");
+            Console.WriteLine($"\tWebRequest3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            webRequest3.GetResponse();
+            Console.WriteLine($"\t tWebRequest3. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            //если закомментить, то передача управления в Main случится раньше, чем выполнятся задачи
+            task.Wait();
+
+            Console.WriteLine($"\tRunAsyncVersion3. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static async Task<string> RunAsyncVersion41()
+        {
+            Console.WriteLine($"\tRunAsyncVersion3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            var task = AsyncMethod();
+
+            var webRequest3 = WebRequest.Create("http://blogs.msdn.com");
+            Console.WriteLine($"\tWebRequest3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            webRequest3.GetResponse();
+            Console.WriteLine($"\tWebRequest3. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            var result = await task;
+            return $"{result} . Check thread Id: {Thread.CurrentThread.ManagedThreadId}";
+        }
+
+        public static Task<string> RunAsyncVersion42()
+        {
+            Console.WriteLine($"\tRunAsyncVersion3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            var task = AsyncMethod();
+
+            var webRequest3 = WebRequest.Create("http://blogs.msdn.com");
+            Console.WriteLine($"\tWebRequest3. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            webRequest3.GetResponse();
+            Console.WriteLine($"\tWebRequest3. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            return task;
+        }
+
+        public static async Task<string> AsyncMethod()
+        {
+            var webRequest1 = WebRequest.Create("http://rsdn.ru");
+            Console.WriteLine($"\t\tWebRequest1. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            // (1)
+            await webRequest1.GetResponseAsync().ConfigureAwait(false);
+            Console.WriteLine($"\t\tWebRequest1. After thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            var webRequest2 = WebRequest.Create("http://gotdotnet.ru");
+            Console.WriteLine($"\t\tWebRequest2. Before thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            // (2)
+            await webRequest2.GetResponseAsync().ConfigureAwait(false);
+            Console.WriteLine($"\t\tWebResponse2. Аfter thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+            return $"Finish AsyncMethod. Thread Id: {Thread.CurrentThread.ManagedThreadId}";
+        }
+    }
+}
